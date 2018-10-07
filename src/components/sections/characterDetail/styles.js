e81624e52b5663357b5845ae90c2864241426596
Jorge Vinaches <jorge.vinaches@gruposantander.com>
2018-10-07 03:21:52 +0200

    Animate hero detail image

--- src/components/sections/characterDetail/styles.js
+++ src/components/sections/characterDetail/styles.js
@@ -7,21 +7,13 @@ export default StyleSheet.create({
     },
     image: {
         width: '100%',
-        height: 200
-    },
-    dataContainer: {
-        padding: 20,
-        flexDirection: 'row'
-    },
-    text: {
-        color: 'white',
     },
     textImage: {
         height: '20%', 
         color: 'white', 
         fontWeight: 'bold', 
         textAlign: 'center', 
-        fontSize: 13, 
-        paddingTop: 4
+        fontSize: 20, 
+        paddingTop: 20
     }
 })
--- src/components/sections/characterDetail/view.js
+++ src/components/sections/characterDetail/view.js
@@ -1,19 +1,34 @@
 import React from 'react'
-import { View, Text, Image, TouchableOpacity, Animated } from 'react-native'
+import { View, Text, Animated } from 'react-native'
 import styles from './styles'
 import { connect } from 'react-redux' 
 
 class CharacterDetail extends React.Component {
 
+    constructor(props) {
+        super(props) 
+        this.state = {
+            animatedHeight: new Animated.Value(0)
+        }
+    }
+
     render() {
+        Animated.timing(
+            this.state.animatedHeight,
+            {
+                toValue: 300,
+                duration: 500,
+            }
+        ).start()
+
         const { character } = this.props
         const image = character.thumbnail.path ? { uri: character.thumbnail.path+'.'+character.thumbnail.extension } : require('../../../resources/placeholder.jpg')
 
         return (
             <View style={styles.container}>
-                <Image
+                <Animated.Image
                     source={image}
-                    style={styles.image}
+                    style={[styles.image, { height: this.state.animatedHeight }]}
                     resizeMode={'contain'}
                 />
                <Text 
